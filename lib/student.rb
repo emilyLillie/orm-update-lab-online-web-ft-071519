c66778ad20be53cab2c9f10be9602db43e5ae9bb
Emily Lillie <emilylillie3@gmail.com>
2019-08-18 20:08:44 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -47,8 +47,12 @@ class Student
     student.save
   end
 
-   def self.new_from_db(array)
-    Student.new(array[1], array[2], array[0])
+   def self.new_from_db(row)
+    name =  row[1]
+    grade = row[2]
+    id = row[0]
+    student = self.new(name, grade, id)
+    student
   end
 
    def self.find_by_name(name)
