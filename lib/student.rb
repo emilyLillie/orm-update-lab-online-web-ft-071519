bbaf7c8bc5281a765304d571bcdd702bd91d51eb
Emily Lillie <emilylillie3@gmail.com>
2019-08-18 20:00:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -47,5 +47,11 @@ class Student
     student.save
     student
   end
+  
+  
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
 
 end
